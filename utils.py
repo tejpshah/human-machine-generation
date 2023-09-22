97bfb0d455c292c064f9c394387ec503debf137d
tejpshah <55666467+tejpshah@users.noreply.github.com>
2023-09-22 14:43:10 -0400

    [Dataset Generation] Updated script to generate story for each summary from a dataframe.

--- utils.py
+++ utils.py
@@ -1,54 +1,130 @@
+import openai 
 import pandas as pd
 from jinja2 import Template
+from config import OPENAI_API_KEY
+from multiprocessing import Pool, cpu_count, current_process
+from tqdm import tqdm 
 
-INPUT_PATH = 'datasets/hcV3-stories.csv'
-OUTPUT_PATH = 'datasets/hcV3-imagined-stories.csv'
+# Set up OpenAI API key
+openai.api_key = OPENAI_API_KEY 
 
-prompt_template_zero_shot = """
-Given a short prompt summary, write an imagined journal entry about an event. 
+# Define file paths
+input_path = 'datasets/hcV3-stories.csv'
+output_path = 'datasets/hcV3-imagined-stories.csv'
 
-Writing instructions:
-- The story must correspond to the summary.
+# Define prompt templates
+system_prompt_template = """
+Given a short prompt summary, write an imagined journal entry about an event. Writing instructions:
+- The story must correspond to the summary. 
 - Pretend the event happened to you, but do not write about something that actually happened to you.
-- Write using first person perspective.
+- Write using first person perspective.  
 - Use the timeline of when the event happened (e.g., "3 weeks ago", "6 months ago").
 
-Summary (from {{ time_since_event }} days ago):
-{{ summary }}
-
 Now, write the journal entry below.:
-- **Reminder** Please make sure to write a story that corresponds to the summary written above.
+- **Reminder** Please make sure to write a story that corresponds to the summary written above. 
 - Don't write a story about something that actually happened to you.
-- Story must be 15-25 sentences and 600-3000 characters including spaces.
+- Story must be 15-25 sentences and 600-3000 characters including spaces.  
 - Do not start off with any salutations (e.g., "Dear Diary") or dates (e.g., 3/1/2021").
 - Do not include the specific date or time of the event in the story.
+"""
+
+user_prompt_template = """
+Summary (from {{ time_since_event }} days ago): {{ summary }}
 
 Generated Story:
 """
 
-def process_and_save_csv(input_path, output_path):
+def filter_and_save_dataset(input_path, output_path):
+  
+  # Load data
+  data = pd.read_csv(input_path)
+  
+  # Filter for 'imagined' stories
+  imagined_stories = data[data['memType'] == 'imagined']
+  
+  # Keep only relevant columns
+  kept_columns = ['AssignmentId', 'story', 'summary', 'timeSinceEvent']
+  imagined_stories = imagined_stories[kept_columns]
+  
+  # Save filtered data to new CSV
+  imagined_stories.to_csv(output_path, index=False)
 
-    # Load the CSV file
-    data = pd.read_csv(input_path)
+  print(f"Saved {imagined_stories.shape[0]} rows to {output_path}")
 
-    # Filter rows where memType is "imagined"
-    filtered_data = data[data['memType'] == 'imagined']
+def generate_story(summary, time_since_event):
 
-    # Keep only the specified columns
-    filtered_data = filtered_data[['AssignmentId', 'story', 'summary', 'timeSinceEvent']]
+  # Render prompt templates
+  system_prompt = Template(system_prompt_template).render()
+  user_prompt = Template(user_prompt_template).render(
+    summary=summary,
+    time_since_event=time_since_event
+  )
 
-    # Save the filtered dataset to a new CSV file
-    filtered_data.to_csv(output_path, index=False)
+  # Generate story
+  response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo", 
+    messages=[
+      {"role": "system", "content": system_prompt},
+      {"role": "user", "content": user_prompt},
+    ]
+  )
 
-    # Print out the number of rows in the filtered dataset
-    print(f"The filtered dataset contains {filtered_data.shape[0]} rows.")
-    print(f"Saved to {output_path}")
+  return response['choices'][0]['message']['content']
 
-def generate_story_prompt(summary, time_since_event):
-    template = Template(prompt_template_zero_shot)
-    return template.render(summary=summary, time_since_event=time_since_event)
+def generate_stories_for_row(row):
+    
+    # Retrieve details from the row
+    assignment_id = row['AssignmentId']
+    summary = row['summary']
+    time_since_event = row['timeSinceEvent']
+
+    # Print the processing status and the core it's being processed on
+    print(f"Processing {assignment_id} on {current_process().name}")
+
+    # Generate the story using the summary and time since event
+    generated_story = generate_story(summary, time_since_event)
+
+    return assignment_id, generated_story
+
+def generate_all_stories(data):
+    num_cpus = cpu_count()
+    print(f"Using {num_cpus} CPU cores for processing.")
+    
+    # Initialize a progress bar
+    pbar = tqdm(total=len(data), desc="Progress")
+    
+    # Save periodically after processing each row
+    results = []
+    with Pool(num_cpus) as pool:
+        for assignment_id, generated_story in pool.imap_unordered(generate_stories_for_row, [row for _, row in data.iterrows()]):
+            results.append((assignment_id, generated_story))
+            
+            # Update the progress bar
+            pbar.update(1)
+            
+            # Save results to CSV every 10 rows processed (you can adjust this number as needed)
+            if len(results) % 250 == 0:
+                temp_df = pd.DataFrame(results, columns=['AssignmentId', 'generated_story'])
+                temp_df.to_csv('datasets/temp_generated_stories.csv', index=False)
+    
+    # Close the progress bar
+    pbar.close()
+    
+    # Save the final results to a new dataframe and return it
+    return pd.DataFrame(results, columns=['AssignmentId', 'generated_story'])
 
 if __name__ == "__main__":
-    process_and_save_csv(INPUT_PATH, OUTPUT_PATH)
-    summary, time_since_event = "My boyfriend and I went to a concert together and had a great time. We met some of my friends there and really enjoyed ourselves watching the sunset.", 90.0
-    print(generate_story_prompt(summary, time_since_event))
+  
+    # Load imagined stories
+    imagined_stories = pd.read_csv("datasets/hcV3-imagined-stories-small.csv")
+
+    # Generate stories for all rows in the dataframe
+    stories_df = generate_all_stories(imagined_stories)
+
+    # Merge the original dataframe with the generated stories on AssignmentId
+    merged_df = imagined_stories.merge(stories_df, on="AssignmentId")
+
+    # Save the updated dataframe with the generated stories to a new CSV file
+    merged_df.to_csv("datasets/hcV3-imagined-stories-with-generated.csv", index=False)
+
+    print(f"Saved {merged_df.shape[0]} rows to datasets/hcV3-imagined-stories-with-generated.csv")
