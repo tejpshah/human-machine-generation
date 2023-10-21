20589afd6823c53a78717133e8e69e12a27eb5a4
tejpshah <55666467+tejpshah@users.noreply.github.com>
2023-10-21 13:02:32 -0400

    Experiment 3 coding zero shot

--- experiment3_analysis.py
+++ experiment3_analysis.py
@@ -1,4 +1,48 @@
+import os 
+import tqdm
+import openai
 import pandas as pd
+from jinja2 import Template
+from config import OPENAI_API_KEY
+from multiprocessing import Pool
+from functools import partial
+import time
+
+# Set up OpenAI API key
+openai.api_key = OPENAI_API_KEY 
+
+CONVERSATION_START = """
+This is the story: 
+{{ story_content }}
+"""
+
+ZERO_SHOT_SYSTEM_PROMPT = """
+Your expertise lies in discerning human-authored vs. AI-generated stories.
+
+Analyze the provided story and determine its origin: AI (1) or human (0). 
+
+Respond only with the answer 0 and 1 and nothing else. 
+
+0 is human, 1 is AI.
+"""
+
+COT_SYSTEM_PROMPT = """
+Your expertise lies in discerning human-authored vs. AI-generated stories.
+
+Analyze the provided story and determine its origin: AI ("AI") or human ("Human").
+
+Firstly, briefly state your criteria for distinguishing AI from human narratives. Next, contrast the story's elements, specifying which lean toward AI or human characteristics. Your comparative analysis is essential. Conclude with: "Therefore, the likely answer is [AI/Human]".
+
+Your detailed yet succinct evaluation is valued. Let's think step by step. 
+"""
+
+COT_OUTPUT_PARSER = """
+Read the evaluation and determine whether the analysis suggests that the story is AI or human.
+
+Respond only with the answer 0 and 1 and nothing else. 
+
+0 is human, 1 is AI.
+"""
 
 def load_data(file_path):
     return pd.read_csv(file_path)
@@ -39,6 +83,27 @@ def generate_data():
     # Save the first 250 rows of the data
     save_data(combined_stories, OUTPUT_FILE, nrows=250)
 
+def zero_shot_prompt_result(story):
+    system_prompt = Template(ZERO_SHOT_SYSTEM_PROMPT).render()
+    user_prompt = Template(CONVERSATION_START).render(story_content=story)
+    
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", 
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ]
+    )
+    return int(response['choices'][0]['message']['content'])
+
+def run_experiment(data, prompt_func, output_file, result_column_name="result"):
+    with Pool(processes=4) as pool:
+        results = list(tqdm.tqdm(pool.imap(prompt_func, data['story_content']), total=len(data)))
+    data[result_column_name] = results
+
+    # Save the result
+    save_data(data, output_file)
+
 if __name__ == "__main__":
-    # generate_data()
-    pass 
+    data = load_data('datasets/experiment3/prompt_engineering.csv')
+    run_experiment(data, zero_shot_prompt_result, 'datasets/experiment3/prompt_engineering_predictions.csv', result_column_name="zero_shot_result")
